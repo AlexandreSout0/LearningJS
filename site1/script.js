a2cf0b1d6403a5be42eb3470b3b5830dc97932ee
HIDROXIDO <soutoinf@gmail.com>
2021-11-26 18:47:19 -0300

    26nov2021

--- site1/script.js
+++ site1/script.js
@@ -1,1 +1,24 @@
-alert('hello')
+var msg = window.document.getElementById('msg');
+var img = window.document.getElementById('imagem');
+var data = new Date();
+//var hora = data.getHours();
+var minutos = data.getMinutes();
+msg.innerHTML = `Agora são ${hora} : ${minutos}`;
+var hora = 13
+if (hora >= 0 && hora < 12)
+{
+    img.src = 'img/manha.png';
+    document.body.style.background = '#fec97c';
+}
+else if (hora >= 12 && hora < 18)
+{
+
+    img.src = 'img/tarde.png';
+    document.body.style.background = '#72a6df';
+}
+else
+{
+    img.src = 'img/noite.png'
+    document.body.style.background = '#6b4e6a';
+
+}
